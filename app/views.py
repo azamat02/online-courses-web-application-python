7dbad6ccf10e50b7e62401bb08d7fe24e4a85fde
kne3011 <nurkenkarkalaev@mail.ru>
2021-05-13 18:10:20 +0600

    rest

---
+++ app/views.py
@@ -0,0 +1,166 @@
+from datetime import timezone
+
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpRequest
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView, ListView, TemplateView
+from .forms import *
+from .serializers import *
+from rest_framework import status
+from rest_framework.decorators import api_view  # making sure to receive Request, add context to Response
+from rest_framework.response import Response  # is needed to return client defined response
+
+
+def index(request):
+    return render(request, "app/index.html")
+
+
+class registerView(CreateView):
+    form_class = SimpleUserForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/registration.html'
+
+
+class GetCourseByID(DetailView):
+    model = Courses
+    template_name = "app/course.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comments_of_course'] = Comment.objects.filter(comment_on_course=self.object.id)
+        crs_obj = self.get_object()
+        crs_obj.save()
+        return context
+
+
+class AllCourses(ListView):
+    template_name = 'app/all_course.html'
+    context_object_name = 'all_course'
+    queryset = Courses.objects.order_by('course_name')
+
+
+def my_courses(request):
+    result = Purchased_Courses.objects.filter(pc_user=request.user.id)
+    return render(request, "app/my_courses.html",
+                  {"result": result, "empty_result": "There is no courses"})
+
+
+def purchase_courses(request, id):
+    if request.method == 'POST':
+        purchase_object = Purchased_Courses(pc_date=timezone.now(),
+                                            pc_user=request.user.id,
+                                            pc_course=id)
+        purchase_object.save()
+        return redirect("app:home")
+
+
+def leave_comment(request, id):
+    if request.method == 'POST' and len(request.POST.get("comment_text")) > 0:
+        print(request.POST.get("comment_text"), type(request.POST.get("comment_text")), type(request.user.id))
+        comment_object = Comment(comment_text=request.POST.get("comment_text"),
+                                 comment_on_course=Courses.objects.get(pk=id),
+                                 comment_user=SimpleUser.objects.get(pk=request.user.id))
+        comment_object.save()
+        return redirect("app:get_course", pk=id)
+    else:
+        return render(request, "app/search.html", {"empty_res": "There is no course"})
+
+
+def rate_course(request, id):
+    if request.method == 'POST':
+        if request.POST.get("rate_val"):
+            print(type(request.POST.get("rate_val")))
+            course_obj = Courses.objects.get(pk=id)
+            course_obj.sum_rating = course_obj.sum_rating + float(request.POST.get("rate_val"))
+            course_obj.counter_rating = course_obj.counter_rating + 1;
+            course_obj.save()
+            return redirect("app:get_course", pk=id)
+        else:
+            return render(request, "app/search.html", {"empty_res": "There is no course"})
+
+
+class ContactsView(TemplateView):
+    template_name = "app/contacts.html"
+
+
+class AboutView(TemplateView):
+    template_name = "app/about.html"
+
+
+class ProfileView(TemplateView):
+    template_name = "registration/user_page.html"
+
+
+class ChangeView(TemplateView):
+    template_name = "register/change_user.html"
+
+
+def search_by_course_text(request):
+    if request.method == "POST" and len(request.POST.get("search_field")) > 0:
+        searching_text = request.POST.get("search_field")
+        return redirect("app:search_success", text=searching_text)
+    else:
+        return render(request, "app/search.html",
+                      {"empty_res": "There is no article"})
+
+
+def search_success(request, text):
+    if len(text) > 0:
+        search_res = Courses.objects.filter(course_name_contains=text)
+        return render(request, "app/search.html",
+                      {"search_res": search_res, "empty_res": "There is no article"})
+
+
+@api_view(['GET', 'POST'])
+def api_courses_list(request, format=None):
+    if request.method == 'GET':
+        courses = Courses.objects.all()
+
+        serializer = CourseSerializer(courses, many=True)
+
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = CourseSerializer(data=request.data, many=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'POST'])
+def api_comment_list(request, format=None):
+    if request.method == 'GET':
+        comment = Comment.objects.all()
+
+        serializer = CommentSerializer(comment, many=True)
+
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = CommentSerializer(data=request.data, many=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def api_comment_details(request, id, format=None):
+
+    cmt = get_object_or_404(Comment, pk=id)
+
+    if request.method == 'GET':
+        serializer = CommentSerializer(cmt)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = CommentSerializer(cmt, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        cmt.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)  # 204 code for No Content
